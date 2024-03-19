046258cc55d8b31e020ceedb3567e9e9a2cf5660
sha-bash <shabashev.va@yandex.ru>
2024-03-19 15:42:21 +0400

    start bayes programm

---
+++ Python_For_Hackers/Bayes/main.py
@@ -0,0 +1,82 @@
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = 'cape_python.png'
+
+SA1_CORNERS = (130, 265, 180, 315)  
+SA2_CORNERS = (80, 255, 130, 305)   
+SA3_CORNERS = (105, 205, 155, 255) 
+
+
+class Search():
+    """"БАйесовская игра "Поиск и спасение" с 3 областями поиска"""
+
+    def __init__(self, name):
+        self.name = name
+        self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
+        if self.img is None:
+            print('Could not load map file {}'.format(MAP_FILE),
+                  file=sys.stderr)
+            sys.exit(1)
+
+    self.area_actual = 0
+    self.sailor_actual = [0, 0]  # As "local" coords within search area
+
+    self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3],
+                            SA1_CORNERS[0] : SA1_CORNERS[2]]
+
+    self.sa2 = self.img[SA2_CORNERS[1] : SA2_CORNERS[3],
+                            SA2_CORNERS[0] : SA2_CORNERS[2]]
+
+    self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3], 
+                            SA3_CORNERS[0] : SA3_CORNERS[2]]
+
+    self.p1 = 0.2
+    self.p2 = 0.5
+    self.p3 = 0.3
+
+    self.sep1 = 0
+    self.sep2 = 0
+    self.sep3 = 0   
+
+    def draw_map(self, last_known):
+        """Display basemap with scale, last known xy location, search areas."""
+
+        cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2)
+        cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        cv.putText(self.img, '50 Nautical Miles', (71, 370),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+
+
+        cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]),
+                     (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '1',
+                   (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]),
+                     (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '2',
+                   (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]),
+                     (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
+        cv.putText(self.img, '3',
+                   (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15),
+                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+
+
+        cv.putText(self.img, '+', (last_known),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '+ = Last Known Position', (274, 355),
+                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(self.img, '* = Actual Position', (275, 370),
+                   cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+
+        cv.imshow('Search Area', self.img)
+        cv.moveWindow('Search Area', 750, 10)
+        cv.waitKey(500)
+
+
